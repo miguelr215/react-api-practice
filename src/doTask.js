b94378bf905162caf83ad59d1a4aa44c0f9b3def
drexelramos <drexelramos@gmail.com>
2020-10-20 05:50:47 -0700

    added chaining

--- src/doTask.js
+++ src/doTask.js
@@ -19,9 +19,40 @@ function doTask(name){
     return p;
 }
 
-doTask('A');
-doTask('B');
-doTask('C');
-doTask('D');
-doTask('E');
-doTask('F');
+// doTask('A');
+// doTask('B');
+// doTask('C');
+// doTask('D');
+// doTask('E');
+// doTask('F');
+
+doTask('A')
+    .then(result => {
+        console.log(result);
+    });
+
+// Chain 2 functions together - C will execute when B is complete
+doTask('B')
+    .then(result => {
+    console.log(result);
+    return doTask('C');
+    })
+    .then(result => {
+        console.log(result);
+    });
+
+// Chain multiple functions together - G will execute will all others are complete
+Promise
+    .all([
+        doTask('D'),
+        doTask('E'),
+        doTask('F')
+    ])
+    .then(results => {
+        results.forEach(result => console.log(result));
+        return doTask('G');
+    })
+    .then(result => {
+        console.log(result);
+    });
+
